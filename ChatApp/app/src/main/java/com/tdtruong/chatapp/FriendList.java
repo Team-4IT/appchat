fd81221e12e121d4fd15444f786f00e7289ffaae
=symap <=symap099@gmail.com>
2019-10-24 01:50:02 +0700

    gg

--- ChatApp/app/src/main/java/com/tdtruong/chatapp/FriendList.java
+++ ChatApp/app/src/main/java/com/tdtruong/chatapp/FriendList.java
@@ -1,14 +1,131 @@
 package com.tdtruong.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FriendList extends AppCompatActivity {
+
+
+    private View friendView;
+    private RecyclerView myFriendList;
+
+    private DatabaseReference ContactRef, UserRef;
+    private FirebaseAuth mAuth;
+    private String currenUserID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+
+        myFriendList = findViewById(R.id.friend_list);
+        myFriendList.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+
+
+
+        mAuth=FirebaseAuth.getInstance();
+        currenUserID=mAuth.getCurrentUser().getUid();
+
+        ContactRef =FirebaseDatabase.getInstance().getReference().child("Contact").child(currenUserID);
+        UserRef=FirebaseDatabase.getInstance().getReference().child("User")
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions options=new FirebaseRecyclerOptions.Builder<Contact>()
+                .setQuery(ContactRef, Contact.class)
+                .build();
+        final FirebaseRecyclerAdapter<Contact, contactViewHolder> adapter
+                =new FirebaseRecyclerAdapter<Contact, contactViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final contactViewHolder holder, int position, @NonNull Contact model) {
+                String userIDs=getRef(position).getKey();
+
+                UserRef.child(userIDs).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild("image")){
+                            String userImage =dataSnapshot.child("image").getValue().toString();
+                            String profileName =dataSnapshot.child("name").getValue().toString();
+                            String profileStatus =dataSnapshot.child("userStatus").getValue().toString();
+
+                            holder.userName.setText(profileName);
+                            holder.userStatus.setText(profileStatus);
+                            Picasso.get().load(userImage).placeholder(R.drawable.profile_image).into(holder.profileImage);
+                        }
+                        else
+                        {
+                            String profileName =dataSnapshot.child("name").getValue().toString();
+                            String profileStatus =dataSnapshot.child("userStatus").getValue().toString();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public contactViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+                  View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.user_display_layout,viewGroup,false);
+                  contactViewHolder viewHolder= new contactViewHolder(view);
+                  return viewHolder;
+            }
+        };
+
+        myFriendList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class contactViewHolder extends RecyclerView.ViewHolder{
+
+
+        TextView userName, userStatus;
+        CircleImageView  profileImage;
+
+
+        public contactViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName=itemView.findViewById(R.id.user_profile_name);
+            userStatus=itemView.findViewById(R.id.user_status);
+            profileImage=itemView.findViewById(R.id.user_profile_image);
+
+        }
     }
 }
--- ChatApp/app/src/main/java/com/tdtruong/chatapp/Profile.java
+++ ChatApp/app/src/main/java/com/tdtruong/chatapp/Profile.java
@@ -1,14 +1,96 @@
 package com.tdtruong.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.telephony.mbms.MbmsErrors;
+import android.text.TextUtils;
+import android.transition.CircularPropagation;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Profile extends AppCompatActivity {
+
+    private Button UpdateAccountSetting;
+    private EditText userName, userStatus;
+    private CircleImageView userProfileImage;
+
+    private String currentUserID ;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        mAuth=FirebaseAuth.getInstance();
+        currentUserID=mAuth.getCurrentUser().getUid();
+        RootRef= FirebaseDatabase.getInstance().getReference();
+
+
+        InitializeFields();
+
+        UpdateAccountSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UpdateSetting();
+            }
+        });
+    }
+
+    private void InitializeFields() {
+    UpdateAccountSetting = findViewById(R.id.update_setting_button);
+    userName = (EditText) findViewById(R.id.set_user_name);
+    userStatus = (EditText) findViewById(R.id.set_profile_status);
+    userProfileImage=(CircleImageView) findViewById(R.id.set_profile_image);
+
+    }
+
+    private void UpdateSetting() {
+        String setUserName=userName.getText().toString();
+        String setStatus=userStatus.getText().toString();
+
+        if (TextUtils.isEmpty(setUserName)) {
+            Toast.makeText(this,"Please write your user name first...",Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(setStatus)) {
+            Toast.makeText(this,"Please write your status..",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            HashMap<String,String> profileMap= new HashMap<>();
+                profileMap.put("uid",currentUserID);
+                profileMap.put("name",setUserName);
+                profileMap.put("status",setStatus);
+             RootRef.child("User").child(currentUserID).setValue(profileMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                 @Override
+                 public void onComplete(@NonNull Task<Void> task) {
+                     if (task.isSuccessful()) {
+                         Toast.makeText(Profile.this, "Profile update successfuly...", Toast.LENGTH_SHORT).show();
+                     } else {
+                         String message = task.getException().toString();
+                         Toast.makeText(Profile.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                     }
+                 }
+             });
+        }
     }
 }
+
+
