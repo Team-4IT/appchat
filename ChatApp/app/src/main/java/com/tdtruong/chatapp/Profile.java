182ec5b0d156630ddd87b0f588b93807c46118c8
=symap <=symap099@gmail.com>
2019-10-25 18:12:29 +0700

    gg

--- ChatApp/app/src/main/java/com/tdtruong/chatapp/Profile.java
+++ ChatApp/app/src/main/java/com/tdtruong/chatapp/Profile.java
@@ -1,8 +1,12 @@
 package com.tdtruong.chatapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.mbms.MbmsErrors;
 import android.text.TextUtils;
@@ -18,6 +22,9 @@ import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.HashMap;
 
@@ -32,6 +39,13 @@ public class Profile extends AppCompatActivity {
     private String currentUserID ;
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
+
+
+
+    private static final int GalleryPick = 1;
+    private StorageReference UserProfileImagesRef;
+    private ProgressDialog loadingBar;
+
 
 
     @Override
@@ -54,7 +68,28 @@ public class Profile extends AppCompatActivity {
                 UpdateSetting();
             }
         });
+
+        RetrieveUserInfo();
+
+
+        userProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GalleryPick);
+            }
+        });
     }
+
+
+
+    private void RetrieveUserInfo() {
+    }
+
+
 
     private void InitializeFields() {
     UpdateAccountSetting = findViewById(R.id.update_setting_button);
@@ -63,10 +98,28 @@ public class Profile extends AppCompatActivity {
     userProfileImage=(CircleImageView) findViewById(R.id.set_profile_image);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==GalleryPick  &&  resultCode==RESULT_OK  &&  data!=null) {
+            Uri ImageUri = data.getData();
 
 
-
-
+            StorageReference filePath = UserProfileImagesRef.child(currentUserID + ".jpg");
+            filePath.putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(Profile.this, "Profile Image uploaded Successfully...", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String message = task.getException().toString();
+                        Toast.makeText(Profile.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
 
     private void UpdateSetting() {
         String setUserName=userName.getText().toString();
